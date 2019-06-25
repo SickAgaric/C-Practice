d025cd87191a2e91f713049e87305e6fd5a17225
ABC <50049963+SickAgaric@users.noreply.github.com>
2019-06-25 22:37:18 +0800

    Update Test.c

--- Test.c
+++ Test.c
@@ -1034,3 +1034,43 @@ void ContactBookPrint(ContactBook* pbook)
 
 	}
 }
+int* twoSum(int* nums, int numsSize, int target)
+{
+    static int a[2]={0};
+    
+
+	for (int i = 0; i < numsSize - 1; i++)
+	{
+		for (int j = i+1; j < numsSize; j++)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+				a[0] = i;
+				a[1] = j;
+				return a;
+			}
+		}
+	}
+	return 0;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
+   int *ptr =NULL;
+	for (int i = 0; i < numsSize - 1; i++)
+	{
+		for (int j = i+1; j < numsSize; j++)
+		{
+			if (nums[i] + nums[j] == target)
+			{
+                ptr = (int *)malloc(sizeof(int)*2);
+				ptr[0] = i;
+				ptr[1] = j;
+                *returnSize = 2;
+				return ptr;
+			}
+		}
+	}
+    *returnSize = 0;
+	return 0;
+}
